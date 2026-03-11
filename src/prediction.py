eeab6ca9802c6bb45d683bd77ee668d2b843d3d2
Pranadh <balpranadh000@gmail.com>
2026-03-11 12:19:34 +0530

    Fixed the path issue

--- src/prediction.py
+++ src/prediction.py
@@ -3,13 +3,22 @@
 
 import pickle
 import numpy as np
+from pathlib import Path
 
 class Insurance_Prediction:
     def __init__(self):
-        with open('C:\\Users\\Pranadh\\OneDrive\\Desktop\\Tek_Projects\\Insurance_Prediction\\artifacts\\scalar.pkl','rb') as f:
-            self.scalar=pickle.load(f)
-        with open('C:\\Users\\Pranadh\\OneDrive\\Desktop\\Tek_Projects\\Insurance_Prediction\\artifacts\\model.pkl','rb') as f:
-            self.model=pickle.load(f)
+        # artifacts_dir = Path(__file__).resolve().parent.parent / "artifacts"
+
+        # with open(artifacts_dir / "scalar.pkl", "rb") as f:
+        #     self.scalar = pickle.load(f)
+        # with open(artifacts_dir / "model.pkl", "rb") as f:
+        #     self.model = pickle.load(f)
+        
+
+        self.scalar = pickle.load(open("artifacts/scalar.pkl", "rb"))
+        
+        self.model = pickle.load(open("artifacts/model.pkl", "rb"))
+        
     
     def prediction_model(self,Age, Annual_Income_LPA, Policy_Term_Years, Sum_Assured_Lakhs):
         input = np.array([[Age, Annual_Income_LPA, Policy_Term_Years, Sum_Assured_Lakhs]])
